db8438ca9ec87e9c7439b2ee627a6da3839dcdea
Ali Akbar <sayaliakbar@gmail.com>
2025-03-06 22:41:36 +0500

    Update Navbar component structure and add navigation links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,9 +6,26 @@ import logo from "../../assets/logo.svg";
 const Navbar = () => {
   return (
     <div className="gpt3__navbar">
-      <div className="gpt__navbar-links">
-        <div className="gpt3__navbar-link_logo">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">What is GPT3?</a>
+          </p>
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#features">Case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
         </div>
       </div>
     </div>
